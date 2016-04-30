55d9040c83bf01f33c098f27df4af703ff60c6db
chriswongtv <chris@cogifire.com>
2016-04-30 09:46:30 -0700

    face + identity recognition

--- app/eyespot/www/js/app.js
+++ app/eyespot/www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('eyespot', ['ionic'])
+angular.module('eyespot', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,105 @@ angular.module('eyespot', ['ionic'])
     }
   });
 })
+
+.controller('AppCtrl', function($scope, $interval, $http, $ionicPlatform) {
+  $scope.liveImg = '';
+
+  $scope.launchCamera = function() {
+    $scope.cameraPlus = cordova.plugins.CameraPlus;
+    $scope.cameraPlus.startCamera(function() {
+      $interval(function() {
+        $scope.cameraPlus.getJpegImage(function(imgData) {
+          $scope.liveImg = 'data:image/jpeg;base64, ' + imgData;
+        }, function() {
+          console.log("error");
+        });
+      }, 100);
+
+      $interval(function() {
+        $scope.cameraPlus.getJpegImage(function(imgData) {
+          var options = {
+            "method": "POST",
+            "url": "https://api.imgur.com/3/image",
+            "headers": {
+              "Authorization": "Bearer ac1920b5b8ebbe3dfbc45afc1a8fc9e949dd0c3a"
+            },
+            "data": { "image": imgData, "album": "OvQIN", "type": "base64" }
+          };
+
+          $http(options).then(function(res) {
+            var imgLink = res.data.data.link;
+            console.log(imgLink);
+
+            var detectConfig = {
+              "method": "POST",
+              "url": "https://api.projectoxford.ai/face/v1.0/detect",
+              "headers": {
+                "Ocp-Apim-Subscription-Key": "90d05fedd28846839d4f6d66c28e091d",
+                "Content-Type": "application/json"
+              },
+              "data": { "url": imgLink }
+            };
+
+            $http(detectConfig).then(function(res) {
+              if (res.data !== []) {
+                res.data.forEach(function(val) {
+                  var faceId = val.faceId;
+
+                  var identifyConfig = {
+                    "method": "POST",
+                    "url": "https://api.projectoxford.ai/face/v1.0/identify",
+                    "headers": {
+                      "content-type": "application/json",
+                      "ocp-apim-subscription-key": "90d05fedd28846839d4f6d66c28e091d"
+                    },
+                    "data": { 
+                      "personGroupId": "eyespot",
+                      "faceIds":[ faceId ],
+                      "maxNumOfCandidatesReturned": 1
+                    }
+                  };
+
+                  $http(identifyConfig).then(function(res) {
+                    console.log(res);
+
+                    if (res.data[0].candidates !== []) {
+                      var personId = res.data[0].candidates[0].personId;
+
+                      var personConfig = {
+                        "method": "GET",
+                        "url": "https://api.projectoxford.ai/face/v1.0/persongroups/eyespot/persons/" + personId,
+                        "headers": {
+                          "ocp-apim-subscription-key": "90d05fedd28846839d4f6d66c28e091d"
+                        }
+                      };
+
+                      $http(personConfig).then(function(res) {
+                        var name = res.data.name;
+                        alert(name + ' is in front of you!');
+                      }, function(err) {
+                        console.log(err);
+                      });
+                    }
+                  }, function(err) {
+                    console.log(err);
+                  });
+                });
+              } else {
+                console.log(res);
+              }
+            }, function(err) {
+              console.log(err);
+            })
+          }, function(err) {
+            console.log(err);
+          });
+        }, function() {
+          console.log("error");
+        });
+      }, 2000);
+    }, function() {
+      console.log("error");
+    });
+  }
+});
